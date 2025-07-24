389194f65f259f898c02eba38a01d3420004150f
张添羽 <2665764951@qq.com>
2025-07-24 21:28:08 +0800

    chore(demo): 添加使用 rust_s3 操作阿里云 OSS 的示例

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,62 @@
+mod s3_client;
+
 use dotenvy::dotenv;
 use std::env;
-use axum::{
-    routing::get,
-    Router,
-};
-use axum::http::StatusCode;
-use axum::response::IntoResponse;
+use std::sync::Arc;
+use axum::{routing::get, Router};
+use axum::body::Body;
+use axum::extract::Extension;
+use axum::http::{header, StatusCode};
+use axum::response::{IntoResponse, Response};
+use s3::Bucket;
 use tracing::{info};
+use bytes::Bytes;
 
 #[tokio::main]
-async fn main() {
-    
+async fn main()->Result<(),Box<dyn std::error::Error>> {
+    dotenv().ok();
+
     // 加载 .env 文件
     dotenv().ok();
-    
+
     tracing_subscriber::fmt::init();
+
+    let bucket = s3_client::init_bucket().await?;
 
     let addr = env::var("SERVER_ADDR").unwrap_or_else(|_| "0.0.0.0:8000".to_string());
 
     let app = Router::new()
-        .route("/", get(root));
+        .route("/", get(root))
+        .layer(Extension(bucket));
+
 
     info!("Listening on {}", addr);
 
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 
+    Ok(())
 }
 
-async fn root()  -> impl IntoResponse {
-    (StatusCode::OK,"Hello, world!")
+async fn root(Extension(bucket): Extension<Arc<Bucket>>) -> impl IntoResponse {
+    match bucket.get_object("test.txt").await {
+        Ok(obj) => {
+            let body = obj.bytes();
+            let response: Response<Body> = Response::builder()
+                .status(StatusCode::OK)
+                .header(header::CONTENT_TYPE, "text/plain")
+                .body(Body::from(body.clone()))
+                .unwrap();
+            response
+        }
+        Err(_) => {
+            let body = Bytes::from("文件不存在");
+            let response: Response<Body> = Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .header(header::CONTENT_TYPE, "text/plain")
+                .body(Body::from(body))
+                .unwrap();
+            response
+        }
+    }
 }
---
+++ src/s3_client.rs
@@ -0,0 +1,29 @@
+use s3::bucket::Bucket;
+use s3::creds::Credentials;
+use s3::region::Region;
+use std::env;
+use std::sync::Arc;
+
+pub async fn init_bucket() -> Result<Arc<Bucket>, Box<dyn std::error::Error>> {
+    dotenvy::dotenv().ok(); // 加载 .env 文件
+    let access_key = env::var("ALIYUN_OSS_ACCESS_KEY")?;
+    let secret_key = env::var("ALIYUN_OSS_SECRET_KEY")?;
+    let region_str = env::var("AWS_REGION")?;
+    let bucket_name = env::var("AWS_BUCKET")?;
+    let endpoint = env::var("AWS_ENDPOINT")?;
+
+    // 阿里云 OSS 的 Endpoint（注意：这里 Region 是手动拼的）
+    let region = Region::Custom {
+        region: region_str, // 自定义区域名
+        endpoint,
+    };
+
+    let bucket_name = bucket_name;
+
+    let credentials = Credentials::new(Some(&access_key), Some(&secret_key), None, None, None)?;
+
+    let bucket = Bucket::new(&bucket_name, region.clone(), credentials.clone())?;
+
+    Ok(Arc::new(*bucket))
+}
+
